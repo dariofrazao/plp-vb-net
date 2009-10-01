d0ae50bbb12d02ee16e947ee3b00f9486ca4d3e0
dsurviver <dsurviver@25fc356c-9724-11de-b6d4-8762f465f3f9>
2009-10-01 01:22:55 +0000

    Campo Minado

---
+++ trunk/Campo Minado/Campo Minado/Casa.vb
@@ -0,0 +1,34 @@
+﻿Public Class Casa : Inherits Button
+    Private ValorCampo As Integer
+    Public Const Bomba = -1
+    Public Const Invalido = -2
+    Public Const Altura = 10
+    Public Const Largura = 10
+
+    Public Sub New()
+        MyBase.New()
+        Me.Valor = Invalido
+        SetSize()
+    End Sub
+
+    Public Sub New(ByVal Valor As Integer)
+        MyBase.New()
+        Me.ValorCampo = Valor
+        SetSize()
+    End Sub
+
+    Private Sub SetSize()
+        Me.Height = Altura
+        Me.Width = Largura
+    End Sub
+
+    Public Property Valor() As Integer
+        Get
+            Return ValorCampo
+        End Get
+        Set(ByVal value As Integer)
+            Me.ValorCampo = value
+        End Set
+    End Property
+
+End Class
---
+++ trunk/Campo Minado/Campo Minado/MainForm.Designer.vb
@@ -0,0 +1,104 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class MainForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip
+        Me.JogoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.NovoJogoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator
+        Me.SairToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.AjudaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.SobreToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.JogoToolStripMenuItem, Me.AjudaToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(284, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'JogoToolStripMenuItem
+        '
+        Me.JogoToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NovoJogoToolStripMenuItem, Me.ToolStripSeparator1, Me.SairToolStripMenuItem})
+        Me.JogoToolStripMenuItem.Name = "JogoToolStripMenuItem"
+        Me.JogoToolStripMenuItem.Size = New System.Drawing.Size(44, 20)
+        Me.JogoToolStripMenuItem.Text = "&Jogo"
+        '
+        'NovoJogoToolStripMenuItem
+        '
+        Me.NovoJogoToolStripMenuItem.Name = "NovoJogoToolStripMenuItem"
+        Me.NovoJogoToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.NovoJogoToolStripMenuItem.Text = "&Novo Jogo"
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(149, 6)
+        '
+        'SairToolStripMenuItem
+        '
+        Me.SairToolStripMenuItem.Name = "SairToolStripMenuItem"
+        Me.SairToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.SairToolStripMenuItem.Text = "&Sair"
+        '
+        'AjudaToolStripMenuItem
+        '
+        Me.AjudaToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SobreToolStripMenuItem})
+        Me.AjudaToolStripMenuItem.Name = "AjudaToolStripMenuItem"
+        Me.AjudaToolStripMenuItem.Size = New System.Drawing.Size(50, 20)
+        Me.AjudaToolStripMenuItem.Text = "&Ajuda"
+        '
+        'SobreToolStripMenuItem
+        '
+        Me.SobreToolStripMenuItem.Name = "SobreToolStripMenuItem"
+        Me.SobreToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
+        Me.SobreToolStripMenuItem.Text = "&Sobre..."
+        '
+        'MainForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 264)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "MainForm"
+        Me.Text = "Campo Minado"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents JogoToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents NovoJogoToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripSeparator1 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents SairToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents AjudaToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents SobreToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+
+End Class
